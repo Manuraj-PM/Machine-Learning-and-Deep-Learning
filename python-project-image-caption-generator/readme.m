aea7a09d7f226a5b37aa64a52dd923eec41b9e9c
Manuraj-PM <91936659+Manuraj-PM@users.noreply.github.com>
2022-08-15 10:52:57 +0530

    Create readme.m

---
+++ python-project-image-caption-generator/readme.m
@@ -0,0 +1,5 @@
+Image Caption Generator with CNN & LSTM (Long Short-Term Memory)
+  
+◦ Studied about the CNN and LSTM to create a working model of an image caption generator.
+◦ Extracted image features from a CNN model (Xcepition) which is pretrained on imagenet data set.
+◦ Applied extracted features to LSTM-RNN to generate the caption of image and evaluated the result.
